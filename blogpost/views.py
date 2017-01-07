074afbdc6890e091b2f1d04d85887487b00f340e
jacky10 <huangjian1993@gmail.com>
2017-01-07 21:31:47 +0800

    add selenium tests

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from blogpost.models import Blogpost
 
 
